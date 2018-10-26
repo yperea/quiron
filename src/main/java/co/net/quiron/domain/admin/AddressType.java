a9be8602f791d58bbca112bffe2ebd1fa4e2754d
Yesid Perea <yesid.perea@outlook.com>
2018-10-26 17:20:34 -0500

    Adding AddressType domain class.

---
+++ src/main/java/co/net/quiron/domain/admin/AddressType.java
@@ -0,0 +1,42 @@
+package co.net.quiron.domain.admin;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * This class represents the Address type domain for the application.
+ */
+@Entity(name = "AddressType")
+@Table(name = "ADDRESSS_TYPES")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class AddressType {
+
+    @Id
+    @Column(name = "AddressTypeID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @NonNull
+    @Column(name = "Name")
+    private String name;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CreatedDate")
+    private Date createdDate;
+
+    @UpdateTimestamp
+    @EqualsAndHashCode.Exclude
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "ModifiedDate")
+    private Date modifiedDate;
+}
