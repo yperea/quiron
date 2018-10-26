c31ccb8f1c9fae659ab6ccf589b1fc0d255bb28b
Yesid Perea <yesid.perea@outlook.com>
2018-10-26 13:48:40 -0500

    Adding Patient javabean to hold patient attributes.

---
+++ src/main/java/co/net/quiron/domain/person/Patient.java
@@ -0,0 +1,60 @@
+package co.net.quiron.domain.person;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Entity(name = "Patient")
+@Table(name = "PATIENTS")
+public class Patient {
+
+    @Id
+    @Column(name = "PatientID")
+    private int id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @JoinColumn(name = "PatientID")
+    private Person person;
+
+    @NonNull
+    @Column(name = "BirthDate")
+    private LocalDate birthDate;
+
+    @NonNull
+    @Column(name = "Gender")
+    private String gender;
+
+    @Column(name = "CompanyID")
+    private int companyId;
+
+    @Column(name = "SubscriberCode")
+    private String subscriberCode;
+
+    @Column(name = "IsPrimarySubscriber")
+    private boolean isPrimarySubscriber;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CreatedDate")
+    private Date createdDate;
+
+    @UpdateTimestamp
+    @EqualsAndHashCode.Exclude
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "ModifiedDate")
+    private Date modifiedDate;
+
+
+
+}
