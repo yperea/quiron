b755de86d367a0d5244c0ba5a033c91c0bdc226d
Yesid Perea <yesid.perea@outlook.com>
2018-10-26 19:38:20 -0500

    Adding AddressManager class application and its Test Class AddressManagerTest. Adding addresses attribute to Class State to implement OneToMany relationship between both entities.

---
+++ src/main/java/co/net/quiron/application/person/AddressManager.java
@@ -0,0 +1,19 @@
+package co.net.quiron.application.person;
+
+import co.net.quiron.application.shared.EntityManager;
+import co.net.quiron.domain.person.Address;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AddressManager extends EntityManager<Address> {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * Instantiates a new Entities manager.
+     */
+    public AddressManager() {
+        super(Address.class);
+        logger.info("AddressManager(): Instantiating AddressManager class.");
+    }
+}
---
+++ src/test/java/co/net/quiron/application/person/AddressManagerTest.java
@@ -0,0 +1,79 @@
+package co.net.quiron.application.person;
+
+import co.net.quiron.application.admin.StateManager;
+import co.net.quiron.application.shared.EntityManager;
+import co.net.quiron.domain.admin.State;
+import co.net.quiron.domain.person.Address;
+import co.net.quiron.test.util.DatabaseManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressManagerTest {
+
+    EntityManager<Address> addressManager;
+    EntityManager<State> stateManager;
+
+    @BeforeEach
+    void setUp() {
+        addressManager = new AddressManager();
+        stateManager = new StateManager();
+        DatabaseManager dbm = DatabaseManager.getInstance();
+        dbm.runSQL("cleandb.sql");
+    }
+
+    @Test
+    void testGetAddressByID() {
+        Address address = addressManager.get(1);
+
+        assertNotNull(address);
+        assertEquals("1701 Wright Street", address.getAddressLine1());
+    }
+
+    @Test
+    void testGetAllAddresses() {
+        List<Address> addressList = addressManager.getList();
+        assertEquals(2,addressList.size());
+    }
+
+    @Test
+    void testCreateAddress() {
+
+        State state = stateManager.get(6);
+        String address1 = "211 North Carroll Street";
+        String city = "Madison";
+        String postalCode = "53703";
+
+        Address newAddress = new Address(address1, city, state, postalCode);
+        Address createAddress = addressManager.create(newAddress);
+
+        assertNotNull(createAddress);
+        assertEquals(newAddress.getAddressLine1(), createAddress.getAddressLine1());
+    }
+
+    @Test
+    void testUpdateAddress() {
+
+        Address address = addressManager.get(1);
+        State state = stateManager.get(5);
+        String address1 = "2125 Commercial Avenue";
+        address.setAddressLine1(address1);
+        address.setState(state);
+        addressManager.update(address);
+        Address updatedAddress = addressManager.get(1);
+
+        assertEquals(address1, updatedAddress.getAddressLine1());
+        assertEquals(address, updatedAddress);
+    }
+
+    @Test
+    void testDeleteAddress() {
+
+        addressManager.delete(addressManager.get(1));
+        assertNull(addressManager.get(1));
+    }
+
+}
